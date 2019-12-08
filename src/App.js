60a0e632a8bffad3cee0ec7066fefb1389934c26
Meowmerry <meow3525@gmail.com>
2019-12-08 22:01:50 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PostList from './component/PostList'
+
+function App() {
+  return (
+    <div >
+        
+      <PostList/>
+
+    </div>
+  );
+}
+
+export default App;
